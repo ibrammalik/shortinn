b1be42a085687e1a89f1452d49621e249fc0fb77
ibrammalik <malikmaul1173@gmail.com>
2024-06-06 13:48:55 +0700

    refactor(component): move Navbar component to separate file (MainNav)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      {/* start of navbar */}
-      <nav className=" fixed w-full z-20 top-0 start-0">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="URL Shortener Logo" width={40} height={40} />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">URL Shortener</span>
-          </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
-            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </nav>
-      {/* end of navbar */}
-
-      {/* start of main section */}
       <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-24">
         <section>
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
@@ -48,10 +26,6 @@ export default function Home() {
           </div>
           <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
         </section>
-
       </main>
-      {/* end of main section */}
-
-    </>
   );
 }
