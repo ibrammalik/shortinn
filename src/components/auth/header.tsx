9301fb2ed6b45d11e619b0a5cb6bb3994f6a846b
ibrammalik <malikmaul1173@gmail.com>
2024-06-10 08:07:14 +0700

    feat(auth): create login form and add schemas for login and register
    - Created a login form component with fields for email and password.
    - Installed react-icons library to use icons for Google and GitHub login buttons.
    - Added social login buttons for Google and GitHub using react-icons.
    - Added Card and Input components from shadcn/ui to structure and style the login form.
    - Added validation schemas for login and register forms to ensure proper input validation.
    
    This update enhances the user authentication experience by providing a visually appealing login form, convenient social login options, and robust input validation for both login and registration.

---
+++ src/components/auth/header.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+interface HeaderProps {
+    title: string;
+    label: string;
+}
+
+export const Header = ({
+    title,
+    label
+}: HeaderProps) => {
+    return (
+        <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+            <h1 className="text-3xl font-semibold">
+                {title}
+            </h1>
+            <p className="text-muted-foreground text-sm">
+                {label}
+            </p>
+        </div>
+    )
+}
